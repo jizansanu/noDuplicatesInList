a979e717f5d59ce9a2a5e0f4e79ea39e536f9326
MOHAMMED JIZAN K <jizansanu@gmail.com>
2019-03-10 00:54:35 +0530

    Create list.py

---
+++ list.py
@@ -0,0 +1,19 @@
+mylist = []
+ne = input("enter number of elements:")
+val = int(ne)
+for x in range (val):
+        elem1 = input("enter list values: \n")
+        elem = str(elem1)
+        mylist.insert(x,elem)
+        x = x + 1
+list2 = []
+for x in range (val):
+        for y in range(val)
+            if (mylist[y] == mylist[x])
+                mylist.pop(y)
+            else list2[y] = insert(mylist[y])
+            y = y + 1
+        x = x + 1
+
+
+print(mylist)
